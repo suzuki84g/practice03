19e01fde4e2a3a1bcdc818dfcf04ab0af9627d3d
suzuki84g <suzuki.84g@gmail.com>
2020-05-12 16:59:07 +0900

    Console手打ちで指定したfigureを表示させる

--- src/js/practice03.js
+++ src/js/practice03.js
@@ -1,1 +1,14 @@
 console.log('hello world!!');
+
+/* menuのスライドショー
+指定の秒数でメニューの画像を変更する。
+div.slidesの中身に.gridBoxがあるので、
+それを順番に表示させていく
+*/
+
+// slide配下のfigureタグが全て配列に格納される
+const slides = document.getElementsByClassName('slides')[0].getElementsByTagName('figure');
+
+function slideImages(figure_no) {
+    slides[figure_no].style.display = 'block';  //0番目の`slide`のdisplayにblockを書き込む（CSSをHTML側で上書きする）
+}
