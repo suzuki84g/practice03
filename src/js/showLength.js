0e47071eab2f370c14e7dee2181edf698a61b783
suzuki84g <suzuki.84g@gmail.com>
2020-05-19 13:05:32 +0900

    リファクタリング

--- src/js/showLength.js
+++ src/js/showLength.js
@@ -1,27 +1,17 @@
 // textareaの文字数を表示させる
-
 function showLength(str) {
     document.getElementById('inputLength').innerHTML = "現在" + str.length + "文字";
 };
 
 // TODO:入力確認ボタンが押されたときの動作
+// form入力内容の取得
 const inputCheck = () => {
-    let fullName = document.getElementById('fullName').value;
-    let phoneNumber = document.getElementById('phoneNumber').value;
-    let email = document.getElementById('email').value;
-    let numOfPeople = document.getElementById('numOfPeople').value;
-    let message = document.getElementById('message').value;
-    let test = [fullName, phoneNumber, email, numOfPeople, message];
-    console.log(test);
+    let fm = document.forms[0];
+    let formObject = new Object();
+
+    for (i = 0; i < fm.elements.length; i++) {
+        var nameTag = fm.elements[i].name;
+        formObject[nameTag] = fm.elements[i].value;
+    };
+    console.log(formObject);  // 動作確認用
 };
-
-// TODO:リファクタリング
-var fm = document.forms[0];
-var formObject = new Object();
-
-for (i = 0; i < fm.elements.length; i++) {
-    var nameTag = fm.elements[i].name;
-    formObject[nameTag] = fm.elements[i].value;
-}
-
-document.forms[0].elements.length
