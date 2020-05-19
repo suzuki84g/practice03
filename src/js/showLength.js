cb3945ed618a150efc8ce0042ea910073f8f3be6
suzuki84g <suzuki.84g@gmail.com>
2020-05-19 14:06:21 +0900

    追加の作業内容の明記

--- src/js/showLength.js
+++ src/js/showLength.js
@@ -14,4 +14,13 @@ const inputCheck = () => {
         formObject[nameTag] = fm.elements[i].value;
     };
     console.log(formObject);  // 動作確認用
+}
+
+// TODO:入力内容の精査、変数のスコープ範囲について確認
+// 名前、電話番号、人数、希望予約時間に空欄が無いか
+const nullCheck = (propertyName) => {
+    formObject[propertyName]
 };
+
+// TODO:エラーの出力
+    // ポップアップのアラートを表示させたい
