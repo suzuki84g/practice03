5c219a0d52114526e42fdac9c3b231b238448bfe
suzuki84g <suzuki.84g@gmail.com>
2020-05-19 11:18:25 +0900

    フォームの情報を取得

--- src/js/showLength.js
+++ src/js/showLength.js
@@ -5,3 +5,12 @@ function showLength(str) {
 };
 
 // TODO:入力確認ボタンが押されたときの動作
+const inputCheck = () => {
+    let fullName = document.getElementById('fullName').value;
+    let phoneNumber = document.getElementById('phoneNumber').value;
+    let email = document.getElementById('email').value;
+    let numOfPeople = document.getElementById('numOfPeople').value;
+    let message = document.getElementById('message').value;
+    let test = [fullName, phoneNumber, email, numOfPeople, message];
+    console.log(test);
+};
