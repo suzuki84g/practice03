7f71e4cbe9b3903c261531421ab86efc7ad66ac6
suzuki84g <suzuki.84g@gmail.com>
2020-05-25 12:50:48 +0900

    Request関連の処理を記述

--- src/js/mediaAPI.js
+++ src/js/mediaAPI.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// TODO:YoutubeMediaAPIの取得の手順を書く
+// `const APIKey = '***'` はconfig.jsに記載
+
+// YoutubeMediaAPIの取得の手順を書く
+
+// TODO:URLを定義
+// GET https://www.googleapis.com/youtube/v3/search
+const requestURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=(チャンネルID)&order=date&key=' + APIKey;
+const response = null;
+
+// TODO:Request投げる
+var request = new XMLHttpRequest();
+request.open = ('GET', requestURL, true);
+
+// TODO:Response戻り時の処理を予め記述
+request.onload = function () {
+    // 処理（代数に一回ぶっこむとか）
+};
+// 実際に送信する
+request.send();
+
+// TODO:HTMLへの反映
+// TODO:処理
