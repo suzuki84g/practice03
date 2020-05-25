5795ad009827934cd832287dc9a2c34e184c235f
suzuki84g <suzuki.84g@gmail.com>
2020-05-25 20:41:47 +0900

    responseの受け取りを確認

--- src/js/mediaAPI.js
+++ src/js/mediaAPI.js
@@ -2,13 +2,13 @@
 
 // 変数の宣言
 let requestURL = null;
-let responseData = null;
+let responseData = new Object;
 let request = new XMLHttpRequest();
 
 // requestのパラメータを設定
 // GET https://www.googleapis.com/youtube/v3/search
 const requestProperty = {
-    URL: 'https://www.googleapis.com/youtube/v3/search',
+    basicURL: 'https://www.googleapis.com/youtube/v3/search',
     part: '?part=snippet',
     q: '&q=coffee',
     order: '&order=relevance',
@@ -18,17 +18,17 @@ const requestProperty = {
 // requestURLを作成する関数
 const requestURLMake = (property) => {
     // `const APIKey = '***'` はconfig.jsに記載
-    requestURL = property.URL + property.part + property.q + property.order + property.key;
+    requestURL = property.basicURL + property.part + property.q + property.order + property.key;
 };
 
 // TODO:Request投げる
-const dataGet = (URL) => {
-    request.open('GET', URL, true);
+const dataGet = (apiURL) => {
+    request.open('GET', apiURL, true);
     request.responseType = 'json';
+    console.log(request.responseType);
     // responseをオブジェクト化する処理
     request.onload = () => {
-        responseData = this.response;
-        console.log(responseData);
+        console.log(request.response);
     };
     // 実際に送信する
     request.send();
@@ -40,3 +40,4 @@ const dataGet = (URL) => {
 // YoutubeMediaAPIの取得の手順を書く
 
 requestURLMake(requestProperty);
+dataGet(requestURL);
