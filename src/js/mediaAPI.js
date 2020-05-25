9b26116b37fcc07ba74a863d730c60f3c1de2bd0
suzuki84g <suzuki.84g@gmail.com>
2020-05-25 14:52:28 +0900

    requestの処理を実装

--- src/js/mediaAPI.js
+++ src/js/mediaAPI.js
@@ -7,18 +7,22 @@
 // TODO:URLを定義
 // GET https://www.googleapis.com/youtube/v3/search
 const requestURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=(チャンネルID)&order=date&key=' + APIKey;
-const response = null;
+const request = new XMLHttpRequest();
+const responseData = null;
 
 // TODO:Request投げる
-var request = new XMLHttpRequest();
-request.open = ('GET', requestURL, true);
-
-// TODO:Response戻り時の処理を予め記述
-request.onload = function () {
-    // 処理（代数に一回ぶっこむとか）
+const dataGet = () => {
+    request.open('GET', requestURL, true);
+    request.responseType = 'json';
+    // TODO:Response戻り時の処理を予め記述
+    request.onload = () => {
+        responseData = this.request;
+        console.log(responseData);
+        // 処理（代数に一回ぶっこむとか）
+    };
+    // 実際に送信する
+    request.send();
 };
-// 実際に送信する
-request.send();
 
 // TODO:HTMLへの反映
 // TODO:処理
